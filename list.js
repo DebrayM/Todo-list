756a0979812e394c6edd7069dbf487b3390af72b
Martine Debray <m.debray@codeur.online>
2021-09-09 09:36:07 +0200

    commit avec design

--- list.js
+++ list.js
@@ -5,12 +5,20 @@ window.onload = function() {
     let liList = "";
     //boucle de création des li
     for (let i=0; i<(list.length); i++) {
-        liList = liList + '<li>' + list[i] + "</li>";
+        liList = liList + '<li><span>' + list[i] + '</span><button type="button">Supprimer</button></li>';
     }
     //intègre les li dans la UL
     document.getElementById("ulList").innerHTML = liList;
 
     listenList();
+
+    let additem = document.getElementById('bnt');
+    additem.addEventListener('click', NewItem);
+
+    let del = document.querySelectorAll("button");
+    for (let i=0; i<del.length; i++) {
+        del[i].addEventListener('click', deleteItem);
+    }
 
 }
 
--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 function listenList() {
     //place des écouteurs d'évènements sur les li
-    let a = document.querySelectorAll("li");
+    let a = document.querySelectorAll("span");
     for (let i=0; i<a.length; i++) {
         a[i].addEventListener('click', editItem);
     }
@@ -9,6 +9,26 @@ function listenList() {
 function editItem() {
     //permet à l'utilisateur de saisir un texte de remplacement
     let resp = prompt("Saisir le texte de remplacement : ");
+    resp = resp.padEnd()
     //remplace la ligne par le texte saisi
     this.innerHTML = resp;
+}
+
+function NewItem() {
+
+    //permet à l'utilisateur de saisir un texte de remplacement
+    let resp = prompt("Saisir le nouvel item : ");
+    if (resp != "" && resp != null) {
+        //modifie le li nouvellement créé dans la UL
+        let parentUl = document.getElementById("ulList");
+        parentUl.innerHTML += '<li><span>' + resp + '</span><button type"button">Supprimer</button></li>';
+    }
+
+}
+
+function deleteItem () {
+    let r = confirm("Voulez-vous vraiment supprimer cet item ?");
+    if (r==true) {
+        this.parentNode.remove();
+    }
 }
